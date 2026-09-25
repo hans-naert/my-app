1057868bd853d3097c69ba60b84974964e64113f
Hans Naert <hans.naert@vives.be>
2026-09-25 09:56:14 +0200

    useEffect to cleanup timer before new render

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
 import './Counter.css'
 function Counter() {
-    let count = 0;
-    setInterval(() => {
-        count++;
+    let [count, setCount] = useState(0);
+    useEffect(() => {
+    const interval = setInterval(() => {
+        setCount(count => count + 1);
+        setCount(count =>count + 1);
         console.log(count);
     }, 1000);
+    return () => clearInterval(interval);
+    });
+
     return <><span className="counter">Hello counter: {count}</span></>
 }
 export default Counter
