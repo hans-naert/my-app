b290417b28cd81b6b733e95f8b172d53abbe7cdb
Hans Naert <hans.naert@vives.be>
2026-09-25 09:43:55 +0200

    add setInterval timer

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,6 +1,10 @@
 import './Counter.css'
 function Counter() {
     let count = 0;
+    setInterval(() => {
+        count++;
+        console.log(count);
+    }, 1000);
     return <><span className="counter">Hello counter: {count}</span></>
 }
 export default Counter
